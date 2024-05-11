33e2dbb79ea8fcadcee56f39c186f982be9434cd
ioumesh <kuwar51@yahoo.com>
2024-05-12 01:22:45 +0530

    fix:patient form

--- src/app/components/PatientForm/AddPatientModal.tsx
+++ src/app/components/PatientForm/AddPatientModal.tsx
@@ -16,6 +16,7 @@ import { UploadOutlined } from "@ant-design/icons";
 import ActionButton from "../ActionButton/ActionButton";
 import Modal from "../Modal/Modal";
 import ActionTab from "../ActionTab/ActionTab";
+import { CheckboxChangeEvent } from "antd/es/checkbox/Checkbox";
 
 const { TextArea } = Input;
 
@@ -25,13 +26,13 @@ const doctorData = [
     id: 1,
     name: "Dr. John Doe",
     mobile: "1234567890",
-    profilePic: "https://example.com/profile1.jpg",
+    profilePic: "images/doctor-1.png",
   },
   {
     id: 2,
     name: "Dr. Jane Smith",
     mobile: "9876543210",
-    profilePic: "https://example.com/profile2.jpg",
+    profilePic: "images/doctor-2.png",
   },
 ];
 
@@ -64,11 +65,13 @@ const AddPatientModal: React.FC = () => {
       .validateFields()
       .then((values) => {
         // Handle form submission
-        message.success("Form submitted");
+        message.success("Patient invited successfully.");
         setVisible(false);
         console.log(values);
       })
       .catch((error) => {
+        message.error("Fill patient details.");
+
         console.error("Form validation failed:", error);
       });
   };
@@ -113,6 +116,16 @@ const AddPatientModal: React.FC = () => {
     });
   };
 
+  // Function to fill WhatsApp number when "Same as Primary Number" checkbox is checked
+  const handlePrimaryNumberChange = (e: CheckboxChangeEvent) => {
+    if (e.target.checked) {
+      const primaryNumberValue = form.getFieldValue("primaryNumber");
+      form.setFieldsValue({ whatsappNumber: primaryNumberValue });
+    } else {
+      form.setFieldsValue({ whatsappNumber: undefined });
+    }
+  };
+
   return (
     <>
       <ActionButton title="Add Patient" handler={showModal} />
@@ -137,85 +150,109 @@ const AddPatientModal: React.FC = () => {
                 setShowPrescriptionUpload(e.target.value === "existing")
               }
             >
-              <Radio value="walking">Walking Patient</Radio>
+              <Radio value="walking">Walk-in Patient</Radio>
               <Radio value="existing">Existing Patient</Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item
-            name="patientImage"
-            label="Patient Image"
-            rules={[{ required: true, message: "Please upload patient image" }]}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <Upload
-              showUploadList={false}
-              onChange={handleImageUpload}
-              beforeUpload={() => false} // Prevent default upload behavior
+            <Form.Item
+              name="patientImage"
+              rules={[
+                { required: true, message: "Please upload patient image" },
+              ]}
             >
-              <div
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  border: "1px solid #d9d9d9",
-                  borderRadius: "4px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  overflow: "hidden",
-                  margin: "0 auto",
-                  marginBottom: "16px",
-                }}
+              <Upload
+                showUploadList={false}
+                onChange={handleImageUpload}
+                beforeUpload={() => false} // Prevent default upload behavior
               >
-                {imagePreview ? (
-                  <img
-                    src={imagePreview}
-                    alt="Patient"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                    }}
-                  />
-                ) : (
-                  <UploadOutlined
-                    style={{ fontSize: "32px", color: "#1890ff" }}
-                  />
-                )}
-              </div>
-            </Upload>
-          </Form.Item>
+                <div
+                  style={{
+                    width: "80px",
+                    height: "100px",
+                    border: "1px solid #d9d9d9",
+                    borderRadius: "10px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    overflow: "hidden",
+                    margin: "0 auto",
+                    marginBottom: "16px",
+                    background: "#D9D9D9",
+                  }}
+                >
+                  {imagePreview ? (
+                    <img
+                      src={imagePreview}
+                      alt="Patient"
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    />
+                  ) : (
+                    <img src="images/person.png" alt="person" />
+                  )}
+                </div>
+              </Upload>
+            </Form.Item>
+          </div>
           <Form.Item
             name="patientName"
             label="Patient Name"
             rules={[{ required: true, message: "Please enter patient name" }]}
           >
-            <Input />
+            <Input placeholder="Enter patient name" />
           </Form.Item>
           <Form.Item
             name="primaryNumber"
-            label="Primary Number"
-            rules={[{ required: true, message: "Please enter primary number" }]}
+            label="Primary Mobile Number *"
+            rules={[
+              { required: true, message: "Please enter primary number" },
+              { len: 10, message: "Primary number must be 10 digits" },
+            ]}
           >
-            <Input />
+            <Input placeholder="Enter mobile number" />
           </Form.Item>
-          <Form.Item name="whatsappNumber" label="WhatsApp Number">
-            <Input />
+          <Form.Item
+            name="whatsappNumber"
+            label="Whatsapp Number *"
+            rules={[
+              { required: true, message: "Please enter Whatsapp number" },
+              { len: 10, message: "whatsapp number must be 10 digits" },
+            ]}
+          >
+            <Input placeholder="Enter whatsapp number" />
           </Form.Item>
           <Form.Item name="sameAsPrimary" valuePropName="checked">
-            <Checkbox>Same as Primary Number</Checkbox>
+            <Checkbox onChange={handlePrimaryNumberChange}>
+              Same as Mobile Number
+            </Checkbox>
           </Form.Item>
           <Form.Item
             name="email"
-            label="Email"
+            label="Email address *"
             rules={[{ required: true, message: "Please enter email" }]}
           >
-            <Input />
+            <Input placeholder="Enter email address" />
           </Form.Item>
 
           <ActionTab title="Schedule Session">
             <>
-              <Form.Item name="sessionType" initialValue="inPerson">
+              <Form.Item
+                name="sessionType"
+                label="Session Type"
+                initialValue="inPerson"
+              >
                 <Radio.Group>
-                  <Radio value="inPerson">In Person</Radio>
+                  <Radio value="inPerson">In-Person</Radio>
                   <Radio value="online">Online</Radio>
                 </Radio.Group>
               </Form.Item>
@@ -234,7 +271,7 @@ const AddPatientModal: React.FC = () => {
 
               <Form.Item
                 name="sessionTime"
-                label="Session Time"
+                label="Session Time Slot"
                 rules={[
                   { required: true, message: "Please select session time" },
                 ]}
@@ -246,7 +283,7 @@ const AddPatientModal: React.FC = () => {
               </Form.Item>
               <Form.Item
                 name="practitionerName"
-                label="Practitioner Name"
+                label="Enter Practitioner Name"
                 rules={[
                   {
                     required: true,
@@ -255,23 +292,18 @@ const AddPatientModal: React.FC = () => {
                 ]}
               >
                 <AutoComplete
+                  placeholder="Select or enter Psychiatrist / Therapist name"
                   options={doctorOptions}
                   onSelect={handleDoctorSelect}
-                  dropdownMatchSelectWidth={false}
-                  dropdownClassName="doctor-autocomplete-dropdown"
+                  // dropdownMatchSelectWidth={false}
+                  // dropdownClassName="doctor-autocomplete-dropdown"
                 />
               </Form.Item>
               <Form.Item
                 name="sessionDetails"
-                label="Session Details"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please enter session details",
-                  },
-                ]}
+                label="Session Details (Optional"
               >
-                <TextArea />
+                <TextArea placeholder="Enter session details here" />
               </Form.Item>
             </>
           </ActionTab>
@@ -281,11 +313,11 @@ const AddPatientModal: React.FC = () => {
               <>
                 <Form.Item
                   name="prescriptionDate"
-                  label="Prescription Date"
+                  label="Date"
                   rules={[
                     {
                       required: true,
-                      message: "Please select prescription date",
+                      message: "Please date",
                     },
                   ]}
                 >
@@ -293,7 +325,7 @@ const AddPatientModal: React.FC = () => {
                 </Form.Item>
                 <Form.Item
                   name="prescriptionImage"
-                  label="Prescription Image"
+                  label="Prescription"
                   rules={[
                     {
                       required: true,
@@ -340,7 +372,7 @@ const AddPatientModal: React.FC = () => {
                 </Form.Item>
                 <Form.Item
                   name="nextFollowUpDate"
-                  label="Next Follow Up Date"
+                  label="Next Follow-up Date"
                   rules={[
                     {
                       required: true,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,15 @@ import AddPatientModal from "./components/PatientForm/AddPatientModal";
 
 export default function Home() {
   return (
-    <div className="">
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Ekyamm</h1>
       <AddPatientModal />
     </div>
   );
